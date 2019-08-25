01b720d91ee88cb7f0adc28a7970f1016cad05ab
Jiří Málek <jiri.malek@gmail.com>
2019-08-25 21:58:51 +0200

    Adds tests for new functions.

--- tests.sh
+++ tests.sh
@@ -46,3 +46,51 @@ assert_raises "logger::init 'D'; logger::log 'A' 'This is a message.'" 1
 assert "{ rm -rf '/tmp/trace.log' && logger::init 'T' '/tmp/trace.log' && rm -f '/tmp/trace.log' && mkdir '/tmp/trace.log' && LANG=en logger::log 'D' 'This is a debug message.'; } 2>&1" "./logger: line 43: /tmp/trace.log: Is a directory\n[$(date +'%d.%m.%Y %H:%M:%S')] C: Cannot write to log file '/tmp/trace.log' anymore.\n[$(date +'%d.%m.%Y %H:%M:%S')] D: This is a debug message."
 
 assert_end "logger::log() - should fail"
+
+# -- logger::level_is_lower - should succeed tests --------------------------------------
+
+assert_raises "logger::level_is_lower 'D' 'C'" 0
+
+assert_end "logger::level_is_lower() - should succeed"
+
+# -- logger::level_is_lower - should fail tests -----------------------------------------
+
+assert_raises "logger::level_is_lower 'X' 'D'" 1
+assert_raises "logger::level_is_lower 'D' 'X'" 1
+
+assert_raises "logger::level_is_lower 'C' 'C'" 1
+assert_raises "logger::level_is_lower 'C' 'D'" 1
+
+assert_end "logger::level_is_lower() - should fail"
+
+# -- logger::level_is_equal - should succeed tests --------------------------------------
+
+assert_raises "logger::level_is_equal 'D' 'D'" 0
+
+assert_end "logger::level_is_equal() - should succeed"
+
+# -- logger::level_is_equal - should fail tests -----------------------------------------
+
+assert_raises "logger::level_is_equal 'X' 'D'" 1
+assert_raises "logger::level_is_equal 'D' 'X'" 1
+
+assert_raises "logger::level_is_equal 'D' 'A'" 1
+assert_raises "logger::level_is_equal 'D' 'C'" 1
+
+assert_end "logger::level_is_equal() - should fail"
+
+# -- logger::level_is_greater - should succeed tests --------------------------------------
+
+assert_raises "logger::level_is_greater 'C' 'D'" 0
+
+assert_end "logger::level_is_greater() - should succeed"
+
+# -- logger::level_is_greater - should fail tests -----------------------------------------
+
+assert_raises "logger::level_is_greater 'X' 'C'" 1
+assert_raises "logger::level_is_greater 'C' 'X'" 1
+
+assert_raises "logger::level_is_greater 'C' 'C'" 1
+assert_raises "logger::level_is_greater 'D' 'C'" 1
+
+assert_end "logger::level_is_greater() - should fail"
