c213c4f1967c4df54fc8beeb5d2ddbf753173099
Jiří Málek <jiri.malek@gmail.com>
2019-08-26 00:28:12 +0200

    Defaults global level to A.

--- tests.sh
+++ tests.sh
@@ -30,7 +30,7 @@ assert_raises "logger::init 'N' '/usr/debug.log'" 1
 assert_end "logger::init() - should fail"
 
 # -- logger::log - should succeed tests ---------------------------------------
-
+assert "logger::log 'D' 'This is a debug message.' 2>&1" "[$(date +'%d.%m.%Y %H:%M:%S')] D: This is a debug message."
 assert "{ logger::init 'N' && logger::log 'D' 'This is a debug message.'; } 2>&1" ""
 assert "{ logger::init 'A' && logger::log 'D' 'This is a debug message.'; } 2>&1" "[$(date +'%d.%m.%Y %H:%M:%S')] D: This is a debug message."
 assert "{ rm -f '/tmp/debug.log'; logger::init 'A' '/tmp/debug.log' && logger::log 'D' 'This is a debug message.' && cat '/tmp/debug.log'; } 2>&1" "[$(date +'%d.%m.%Y %H:%M:%S')] D: This is a debug message."
@@ -39,11 +39,10 @@ assert_end "logger::log() - should succeed"
 
 # -- logger::log - should fail tests ------------------------------------------
 
-assert_raises "logger::log 'D' 'This is a debug message.'" 1
 assert_raises "logger::init 'D'; logger::log 'X' 'This is a message.'" 1
 assert_raises "logger::init 'D'; logger::log 'A' 'This is a message.'" 1
 
-assert "{ rm -rf '/tmp/trace.log' && logger::init 'T' '/tmp/trace.log' && rm -f '/tmp/trace.log' && mkdir '/tmp/trace.log' && LANG=en logger::log 'D' 'This is a debug message.'; } 2>&1" "./logger: line 43: /tmp/trace.log: Is a directory\n[$(date +'%d.%m.%Y %H:%M:%S')] C: Cannot write to log file '/tmp/trace.log' anymore.\n[$(date +'%d.%m.%Y %H:%M:%S')] D: This is a debug message."
+assert "{ rm -rf '/tmp/trace.log' && logger::init 'T' '/tmp/trace.log' && rm -f '/tmp/trace.log' && mkdir '/tmp/trace.log' && LANG=en logger::log 'D' 'This is a debug message.'; } 2>&1" "./logger: line 45: /tmp/trace.log: Is a directory\n[$(date +'%d.%m.%Y %H:%M:%S')] C: Cannot write to log file '/tmp/trace.log' anymore.\n[$(date +'%d.%m.%Y %H:%M:%S')] D: This is a debug message."
 
 assert_end "logger::log() - should fail"
 
